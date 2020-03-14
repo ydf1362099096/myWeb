a230ec3c115f93bcb0835074bb7fca58b413d5ee
Andy <ydf1362099096@outlook.com>
2020-03-14 20:46:51 +0800

    init repo

---
+++ src/main/java/top/andypage/page/webpage/helloController/helloControl.java
@@ -0,0 +1,17 @@
+package top.andypage.page.webpage.helloController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class helloControl {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name") String name, Model model ){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
---
+++ src/test/java/top/andypage/page/webpage/WebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package top.andypage.page.webpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
